234a78035210a00563307381c65951c4ae24c82e
Mac <effenrilagung300@gmail.com>
2026-06-01 17:10:06 +0700

    fix(bug) mjs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,8 +3,6 @@ module.exports = {
       {
         name: 'nuxt-app', // Ganti dengan nama aplikasi Anda
         port: '3003', // Port di mana Nuxt akan berjalan (sesuaikan jika perlu)
-        script: './node_modules/nuxt/bin/nuxt.js',
-        args: 'start',
         env: {
           NODE_ENV: 'production',
         }
